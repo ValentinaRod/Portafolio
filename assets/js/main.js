17e9ad9d56fe1332235edfb68011621ae1380d92
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-08-05 18:14:08 -0400

    añadiendo archivos casi listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,50 +3,7 @@ $(document).ready(function(){
   $('.slider').slider();
 });
 
-//contacto------------------------------------>
 
-// Input Lock
-$('textarea').blur(function () {
-    $('#hire textarea').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('textarea + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('textarea + label + span').css({'opacity': 0});
-        }
-    });
-});
-
-$('#hire .field:first-child input').blur(function () {
-    $('#hire .field:first-child input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 0});
-        }
-    });
-});
-
-$('#hire .field:nth-child(2) input').blur(function () {
-    $('#hire .field:nth-child(2) input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-        }
-    });
-});
 //home--------------------------------------------------------------->
 
 $(document).ready(function(){
@@ -129,6 +86,10 @@ $(window).scroll(function() {
 });
 });  
 
-//social networks-------------------------------------------------------->
-//$(document).ready(function() {
- 
+//efectos smoove para clases------------------------------------------------------->
+$('.bio').smoove({
+     offset  : '100%',
+    // moveX is overridden to -200px for ".bar" object
+    moveX   : '100px',
+    moveY   : '100px',
+});
--- dist/js/script.js
+++ dist/js/script.js
@@ -20258,50 +20258,7 @@ $(document).ready(function(){
   $('.slider').slider();
 });
 
-//contacto------------------------------------>
 
-// Input Lock
-$('textarea').blur(function () {
-    $('#hire textarea').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('textarea + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('textarea + label + span').css({'opacity': 0});
-        }
-    });
-});
-
-$('#hire .field:first-child input').blur(function () {
-    $('#hire .field:first-child input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:first-child input + label + span').css({'opacity': 0});
-        }
-    });
-});
-
-$('#hire .field:nth-child(2) input').blur(function () {
-    $('#hire .field:nth-child(2) input').each(function () {
-        $this = $(this);
-        if ( this.value != '' ) {
-          $this.addClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 1});
-        }
-        else {
-          $this.removeClass('focused');
-          $('.field:nth-child(2) input + label + span').css({'opacity': 0});
-        }
-    });
-});
 //home--------------------------------------------------------------->
 
 $(document).ready(function(){
@@ -20383,4 +20340,6 @@ $(window).scroll(function() {
  
 });
 });  
+
+//social networks-------------------------------------------------------->
 
